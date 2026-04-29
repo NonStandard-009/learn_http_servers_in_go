6ec12dd3e6948f67a4e725b361b8c8c95e540b0b
NonStandard-009 <sebaslr.2002@proton.me>
2026-04-29 18:42:56 -0500

    Refactored: handlers now live in 'handlers.go'

---
+++ handlers.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	fmt.Fprintf(w, "Resetting hit counter\n")
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Printf("Error writting response: %v\n", err)
+	}
+}
--- main.go
+++ main.go
@@ -1,54 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"sync/atomic"
 )
 
-const projectRoot = "/home/thanatos/Workspace/boot_dev/back_end_dev_path/learn_http_servers_in_go/"
 const port = "8080"
 
-type apiConfig struct {
-	fileserverHits atomic.Int32
-}
-
-func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits.Load())
-}
-
-func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	cfg.fileserverHits.Store(0)
-	fmt.Fprintf(w, "Resetting hit counter\n")
-}
-
-func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cfg.fileserverHits.Add(1)
-		next.ServeHTTP(w, r)
-	})
-}
-
-func serverHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.WriteHeader(http.StatusOK)
-	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
-	if err != nil {
-		log.Printf("Error writting response: %v\n", err)
-	}
-}
-
 func main() {
-	handler := http.StripPrefix("/app", http.FileServer(http.Dir(projectRoot)))
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
 	var cfg apiConfig
 
 	mux := http.NewServeMux()
 	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
 
-	mux.HandleFunc("/healthz", serverHandler)
+	mux.HandleFunc("/healthz", healthzHandler)
 	mux.HandleFunc("/metrics", cfg.metricsHandler)
 	mux.HandleFunc("/reset", cfg.resetHandler)
 
@@ -57,6 +23,6 @@ func main() {
 		Addr:    ":" + port,
 	}
 
-	log.Printf("Serving files from '%s' on port: %s\n", projectRoot, port)
+	log.Printf("Serving files from '/' on port: %s\n", port)
 	log.Fatal(server.ListenAndServe())
 }
