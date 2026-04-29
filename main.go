8abcc5ab5e3158172f066ef7f837975d79a1fb9f
NonStandard-009 <sebaslr.2002@proton.me>
2026-04-29 16:46:17 -0500

    Changed fileserver to serve from `/app`

--- main.go
+++ main.go
@@ -8,9 +8,21 @@ import (
 const projectRoot = "/home/thanatos/Workspace/boot_dev/back_end_dev_path/learn_http_servers_in_go/"
 const port = "8080"
 
+func serverHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Printf("Error writting response: %v\n", err)
+	}
+}
+
 func main() {
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(projectRoot)))
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(projectRoot)))
+	mux.Handle("/app/", handler)
+	mux.HandleFunc("/healthz", serverHandler)
 
 	server := &http.Server{
 		Handler: mux,
