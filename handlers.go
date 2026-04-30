07797c6641cb41c86bf77189595dec4ad7415044
NonStandard-009 <sebaslr.2002@proton.me>
2026-04-30 14:33:09 -0500

    Updated metrics handler

--- handlers.go
+++ handlers.go
@@ -12,8 +12,19 @@ type apiConfig struct {
 }
 
 func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(w, "Hits: %d\n", cfg.fileserverHits.Load())
+	htmlContent := fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`,
+		cfg.fileserverHits.Load())
+
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := w.Write([]byte(htmlContent))
+	if err != nil {
+		log.Printf("Error writting response: %v\n", err)
+	}
 }
 
 func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
